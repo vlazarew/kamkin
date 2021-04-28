32432da9b0ac880dc50df62efbd7b882d5a1c05c
Vladimir Lazarev <Lazarewvladimir18@gmail.com>
2021-04-28 16:01:48 +0300

    some work

--- hw/lab1/main.cpp
+++ hw/lab1/main.cpp
@@ -4,20 +4,36 @@
 #include "fsm.h"
 
 
+void checkNextFormulaNode(std::vector<std::pair<const model::ltl::Formula &, int>> &fixedFormula, int level,
+                          bool needToCheckOnCurrentLevel);
+
+bool canAddFormulaToVector(const model::ltl::Formula &source, std::vector<model::ltl::Formula> &result);
+
 using namespace model::ltl;
 using namespace model::fsm;
 using namespace std;
 
-void getAllFormulas(const Formula &source, std::vector<Formula> &result, std::vector<Formula> &atoms) {
+void getClosure(const Formula &source, std::vector<Formula> &result, std::vector<Formula> &atoms) {
     if (&source.lhs() != nullptr) {
         result.push_back(source);
-        getAllFormulas(source.lhs(), result, atoms);
+        getClosure(source.lhs(), result, atoms);
     }
 
     if (&source.rhs() != nullptr) {
-        getAllFormulas(source.rhs(), result, atoms);
+        getClosure(source.rhs(), result, atoms);
     }
 
+    bool canAddSource = canAddFormulaToVector(source, result);
+
+    if (canAddSource) {
+        result.push_back(source);
+        if (source.kind() == model::ltl::Formula::ATOM) {
+            atoms.push_back(source);
+        }
+    }
+}
+
+bool canAddFormulaToVector(const Formula &source, vector<Formula> &result) {
     bool canAddSource = true;
     for (const auto &tempFormula : result) {
         if (&tempFormula.lhs() == &source.lhs()
@@ -29,68 +45,134 @@ void getAllFormulas(const Formula &source, std::vector<Formula> &result, std::ve
             break;
         }
     }
+    return canAddSource;
+}
 
-    if (canAddSource) {
-        result.push_back(source);
-        if (source.kind() == model::ltl::Formula::ATOM) {
-            atoms.push_back(source);
+bool canAddFormulaToVector(const Formula &source, int level, vector<pair<const Formula &, int>> &result) {
+    bool canAddSource = true;
+    for (const auto &tempFormula : result) {
+        if (&tempFormula.first == &source
+            && &tempFormula.first.lhs() == &source.lhs()
+            && &tempFormula.first.rhs() == &source.rhs()
+            && &tempFormula.first.arg() == &source.arg()
+            && tempFormula.first.kind() == source.kind()
+            && tempFormula.first.prop() == source.prop()
+            && level == tempFormula.second) {
+            canAddSource = false;
+            break;
         }
     }
+    return canAddSource;
 }
 
-void updateFormulaMap(const Formula &formula, vector<pair<Formula::Kind, string>> &map) {
-    if (&formula != nullptr) {
-        map.push_back(pair<Formula::Kind, string>(formula.kind(), formula.prop()));
-    }
+//void updateFormulaMap(const Formula &formula, vector<pair<Formula::Kind, string>> &map) {
+//    if (&formula != nullptr) {
+//        map.push_back(pair<Formula::Kind, string>(formula.kind(), formula.prop()));
+//    }
+//
+//    if (&formula.lhs() != nullptr) {
+//        updateFormulaMap(formula.lhs(), map);
+//    }
+//
+//    if (&formula.rhs() != nullptr) {
+//        updateFormulaMap(formula.rhs(), map);
+//    }
+//}
 
-    if (&formula.lhs() != nullptr) {
-        updateFormulaMap(formula.lhs(), map);
-    }
 
-    if (&formula.rhs() != nullptr) {
-        updateFormulaMap(formula.rhs(), map);
-    }
-}
-
-void simplifyFormula(const Formula &formula) {
+void simplifyFormula(const Formula &formula, vector<pair<const Formula &, int>> &fixedFormula, int level = 0) {
     if (&formula == nullptr) {
         return;
     }
 
-    if (&formula.lhs() != nullptr) {
-        simplifyFormula(formula.lhs());
-    }
-
-    if (&formula.rhs() != nullptr) {
-        simplifyFormula(formula.rhs());
-    }
-
-    if (formula.kind() == Formula::IMPL) {
-        int i = 1;
-//        formula.lhs() = !(formula.lhs());
-//        formula.lhs(). = &a;
+    bool edited = false;
+    if (formula.kind() == Formula::IMPL && !edited) {
         Formula a = formula.lhs().operator!();
-        const Formula &b = a.operator||(formula.rhs());
-//        formula = b;
-        auto *p = const_cast<Formula *>(&formula);
-        const Formula *pb = &b;
-//        formula = &pb;
-//        p = pb;
-        &formula = reinterpret_cast<const Formula *>(p);
-        int j = 1;
+        Formula b = a.operator||(formula.rhs());
+        edited = true;
+        if (canAddFormulaToVector(b, level, fixedFormula)) {
+            fixedFormula.push_back(pair<Formula, int>(b, level));
+            checkNextFormulaNode(fixedFormula, level, true);
+        }
     }
 
+    if (formula.kind() == Formula::G && !edited) {
+        const Formula &a = !F(!formula.lhs());
+        edited = true;
+        if (canAddFormulaToVector(a, level, fixedFormula)) {
+            fixedFormula.push_back(pair<const Formula &, int>(a, level));
+            checkNextFormulaNode(fixedFormula, level, true);
+        }
+    }
+
+    if (formula.kind() == Formula::F && !edited) {
+        const Formula &a = U(P("1"), formula.lhs());
+        edited = true;
+        if (canAddFormulaToVector(a, level, fixedFormula)) {
+            fixedFormula.push_back(pair<const Formula &, int>(a, level));
+            checkNextFormulaNode(fixedFormula, level, true);
+        }
+    }
+
+    if (!edited) {
+        if (canAddFormulaToVector(formula, level, fixedFormula)) {
+            fixedFormula.push_back(pair<const Formula &, int>(formula, level));
+            checkNextFormulaNode(fixedFormula, level, false);
+        }
+    }
 
 }
 
+void checkNextFormulaNode(vector<pair<const Formula &, int>> &fixedFormula, int level, bool needToCheckOnCurrentLevel) {
+    const Formula &tempFormula = fixedFormula.at(fixedFormula.size() - 1).first;
+
+    if (needToCheckOnCurrentLevel && &tempFormula != nullptr) {
+        simplifyFormula(tempFormula, fixedFormula, level);
+    }
+
+    if (&tempFormula.lhs() != nullptr) {
+        simplifyFormula(tempFormula.lhs(), fixedFormula, level + 1);
+    }
+
+    if (&tempFormula.rhs() != nullptr) {
+        simplifyFormula(tempFormula.rhs(), fixedFormula, level + 1);
+    }
+}
+
 void createCopyFormula(const Formula &formula) {
-    vector<pair<Formula::Kind, string>> formulaMap;
+    vector<pair<const Formula &, int>> fixedFormula;
+//    fixedFormula.resize(1);
+//    vector<Formula> fixedFormula;
 //    updateFormulaMap(formula, formulaMap);
-    simplifyFormula(formula);
+    simplifyFormula(formula, fixedFormula, 0);
     int i = 1;
 //    return formula.kind();
 }
 
+vector<Formula> getElem(vector<Formula> closure) {
+    int i = 0;
+    vector<Formula> result;
+    while (i < closure.size()) {
+        Formula formula = closure.at(i);
+        if (formula.kind() == Formula::X) {
+            if (formula.arg().kind() == model::ltl::Formula::NOT) {
+                closure.push_back(X(formula.arg().arg()));
+            } else if (formula.arg().kind() == model::ltl::Formula::OR) {
+                closure.push_back(X(formula.arg().lhs()));
+                closure.push_back(X(formula.arg().rhs()));
+            } else {
+                result.push_back(formula);
+            }
+        }
+
+        if (formula.kind() == model::ltl::Formula::ATOM){
+            result.push_back(formula);
+        }
+        i++;
+    }
+
+    return result;
+}
 
 int main() {
     setlocale(LC_ALL, "");
@@ -105,10 +187,10 @@ int main() {
     std::vector<Formula> allFormulas;
     std::vector<Formula> allAtoms;
     if (&formula.lhs() != nullptr) {
-        getAllFormulas(formula.lhs(), allFormulas, allAtoms);
+        getClosure(formula.lhs(), allFormulas, allAtoms);
     }
     if (&formula.rhs() != nullptr) {
-        getAllFormulas(formula.rhs(), allFormulas, allAtoms);
+        getClosure(formula.rhs(), allFormulas, allAtoms);
     }
 
     std::cout << "Все подвыражения: " << std::endl;
@@ -122,6 +204,16 @@ int main() {
     std::cout << "Атомы: " << std::endl;
     for (int i = 0; i < allAtoms.size(); i++) {
         std::cout << allAtoms.at(i) << ((i + 1) == allAtoms.size() ? "" : ", ");
+    }
+
+    cout << endl;
+    cout << endl;
+
+
+    std::vector<Formula> elem = getElem(allFormulas);
+    std::cout << "getElem: " << std::endl;
+    for (int i = 0; i < elem.size(); i++) {
+        std::cout << elem.at(i) << ((i + 1) == elem.size() ? "" : ", ");
     }
 
     cout << endl;
