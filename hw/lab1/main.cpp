f152240cfeb06074e31c4d8557ebd3845d50f806
Vladimir Lazarev <Lazarewvladimir18@gmail.com>
2021-04-29 01:41:17 +0300

    bad deal

--- hw/lab1/main.cpp
+++ hw/lab1/main.cpp
@@ -165,7 +165,7 @@ vector<Formula> getElem(vector<Formula> closure) {
             }
         }
 
-        if (formula.kind() == model::ltl::Formula::ATOM){
+        if (formula.kind() == model::ltl::Formula::ATOM) {
             result.push_back(formula);
         }
         i++;
@@ -174,52 +174,105 @@ vector<Formula> getElem(vector<Formula> closure) {
     return result;
 }
 
+Formula simplify(Formula formula, vector<Formula> &temp) {
+    if (formula.kind() == model::ltl::Formula::F) {
+        Formula a = U(P("1"), formula.lhs());
+        temp.push_back(a);
+        return U(P("1"), simplify(formula.lhs(), temp));
+    } else if (formula.kind() == model::ltl::Formula::G) {
+        Formula a = F(!(formula.lhs()));
+        temp.push_back(a);
+        return simplify(F(!(formula.lhs())), temp);
+    } else if (formula.kind() == model::ltl::Formula::X) {
+        Formula a = X(formula.lhs());
+        temp.push_back(a);
+        return X(simplify(formula.lhs(), temp));
+    } else if (formula.kind() == model::ltl::Formula::ATOM) {
+        switch (formula.kind()) {
+            case model::ltl::Formula::NOT: {
+                Formula a = !formula.lhs();
+                temp.push_back(a);
+                return !(simplify(formula.lhs(), temp));
+            }
+            case model::ltl::Formula::AND: {
+                Formula a = formula.lhs() && formula.rhs();
+                temp.push_back(a);
+                return simplify(formula.lhs(), temp) && simplify(formula.rhs(), temp);
+            }
+            case model::ltl::Formula::OR: {
+                Formula a = formula.lhs() || formula.rhs();
+                temp.push_back(a);
+                return simplify(formula.lhs(), temp) || simplify(formula.rhs(), temp);
+            }
+            default:
+                return formula;
+        }
+    } else if (formula.kind() == model::ltl::Formula::R) {
+        Formula a = !U(!formula.lhs(), !formula.rhs());
+        temp.push_back(a);
+        return !U(!simplify(formula.lhs(), temp), !simplify(formula.rhs(), temp));
+    } else if (formula.kind() == model::ltl::Formula::NOT) {
+        Formula a = !(formula.lhs());
+        temp.push_back(a);
+        return !(simplify(formula.lhs(), temp));
+    } else if (formula.kind() == model::ltl::Formula::IMPL) {
+        Formula a = (formula.lhs()).operator!().operator||(formula.rhs());
+        temp.push_back(a);
+        return (simplify(formula.lhs(), temp)).operator!().operator||(simplify(formula.rhs(), temp));
+    }
+    return formula;
+}
+
+
 int main() {
     setlocale(LC_ALL, "");
-    const Formula &formula = G(P("p") >> F(P("q")));
+//    const Formula &formula = G(P("p") >> F(P("q")));
+
+    vector<Formula> temp;
+    Formula formula = simplify(G(P("p") >> F(P("q"))), temp);
 
     std::cout << "Formula: " << std::endl;
     std::cout << formula << std::endl << std::endl;
 
 //    Formula simplifiedFormula = createCopyFormula(formula);
-    createCopyFormula(formula);
+//    createCopyFormula(formula);
 
     std::vector<Formula> allFormulas;
     std::vector<Formula> allAtoms;
-    if (&formula.lhs() != nullptr) {
-        getClosure(formula.lhs(), allFormulas, allAtoms);
-    }
-    if (&formula.rhs() != nullptr) {
-        getClosure(formula.rhs(), allFormulas, allAtoms);
-    }
-
-    std::cout << "Все подвыражения: " << std::endl;
-    for (int i = 0; i < allFormulas.size(); i++) {
-        std::cout << allFormulas.at(i) << ((i + 1) == allFormulas.size() ? "" : ", ");
-    }
-
-    cout << endl;
-    cout << endl;
-
-    std::cout << "Атомы: " << std::endl;
-    for (int i = 0; i < allAtoms.size(); i++) {
-        std::cout << allAtoms.at(i) << ((i + 1) == allAtoms.size() ? "" : ", ");
-    }
-
-    cout << endl;
-    cout << endl;
-
-
-    std::vector<Formula> elem = getElem(allFormulas);
-    std::cout << "getElem: " << std::endl;
-    for (int i = 0; i < elem.size(); i++) {
-        std::cout << elem.at(i) << ((i + 1) == elem.size() ? "" : ", ");
-    }
-
-    cout << endl;
-    cout << endl;
-
-    int i = 1;
+//    if (&formula.lhs() != nullptr) {
+//        getClosure(formula.lhs(), allFormulas, allAtoms);
+//    }
+//    if (&formula.rhs() != nullptr) {
+//        getClosure(formula.rhs(), allFormulas, allAtoms);
+//    }
+//
+//    std::cout << "Все подвыражения: " << std::endl;
+//    for (int i = 0; i < allFormulas.size(); i++) {
+//        std::cout << allFormulas.at(i) << ((i + 1) == allFormulas.size() ? "" : ", ");
+//    }
+//
+//    cout << endl;
+//    cout << endl;
+//
+//    std::cout << "Атомы: " << std::endl;
+//    for (int i = 0; i < allAtoms.size(); i++) {
+//        std::cout << allAtoms.at(i) << ((i + 1) == allAtoms.size() ? "" : ", ");
+//    }
+//
+//    cout << endl;
+//    cout << endl;
+//
+//
+//    std::vector<Formula> elem = getElem(allFormulas);
+//    std::cout << "getElem: " << std::endl;
+//    for (int i = 0; i < elem.size(); i++) {
+//        std::cout << elem.at(i) << ((i + 1) == elem.size() ? "" : ", ");
+//    }
+//
+//    cout << endl;
+//    cout << endl;
+//
+//    int i = 1;
 
     /*Automaton automaton;
     automaton.add_state("s0");
