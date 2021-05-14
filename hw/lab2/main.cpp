3b23c5f069b60119573720c4adc6a6a4c34112a7
Vladimir Lazarev <Lazarewvladimir18@gmail.com>
2021-05-14 11:14:10 +0300

    final version

--- hw/lab2/main.cpp
+++ hw/lab2/main.cpp
@@ -12,7 +12,6 @@
  * the License.
  */
 
-#include <cassert>
 #include <iostream>
 #include <set>
 #include <unordered_set>
@@ -20,7 +19,6 @@
 #include<string>
 #include<cmath>
 #include<stack>
-#include<cstring>
 #include<functional>
 
 #include "bdd.h"
@@ -54,14 +52,6 @@ bool getResultOfRow(bool left, bool right, Formula::Kind boolOperator);
 std::vector<Node> &
 getRootsRecur(Formula &formula1, vector<int> &query, vector<unordered_map<Formula, Formula>> &variablesCombinations,
               vector<Node> &roots, unordered_map<Formula, Formula> &neededs, vector<int> &mainQuery, int level);
-
-//void handleRootSite(const Formula &formula, vector<Node> &vector, vector <Formula> &vector1, unordered_set<Formula> set,
-//                    const Formula formula1, vector<int> &vector2, unordered_map<Formula, Formula> &map);
-
-void
-handleRootSite(Formula &formula1, vector<Node> &roots, unordered_map<Formula, Formula> &neededs,
-               unordered_set<Formula> &variablesTemp, const Formula &site, vector<int> &query,
-               unordered_map<Formula, Formula> &map);
 
 bool canAdd(const Formula &formula, const unordered_map<Formula, Formula> &ignored) {
     for (auto &i : ignored) {
@@ -118,21 +108,6 @@ vector<unordered_map<Formula, Formula> > makeVariablesCombinations(unordered_set
     return combinations;
 }
 
-//void getAllOperators(Formula formula, stack<Formula> &operators) {
-//    if (formula.kind() == Formula::AND || formula.kind() == Formula::EQ || formula.kind() == Formula::IMPL ||
-//        formula.kind() == Formula::OR || formula.kind() == Formula::XOR) {
-//        operators.push(formula.kind());
-//    }
-//
-//    if (&formula.lhs() != nullptr) {
-//        getAllOperators(formula.lhs(), operators);
-//    }
-//
-//    if (&formula.rhs() != nullptr) {
-//        getAllOperators(formula.rhs(), operators);
-//    }
-//}
-
 void
 getResultForCurrentRow(unordered_map<Formula, Formula> currentRow, Formula &formula, vector<Formula> &stackFormulas) {
     if (IsOperator(formula)) {
@@ -171,9 +146,9 @@ getResultForCurrentRow(unordered_map<Formula, Formula> currentRow, Formula &form
     }
 
     if (formula.kind() == Formula::NOT) {
-        Formula tempFormula = currentRow.at(formula.lhs());
-        bool tempBool = tempFormula.kind() == Formula::TRUE;
-        stackFormulas.emplace_back(tempBool ? Formula::F : Formula::T);
+        Formula left = formula.lhs();
+        getResultForCurrentRow(currentRow, left, stackFormulas);
+        stackFormulas.emplace_back(stackFormulas.at(stackFormulas.size() - 1).kind() == Formula::T.kind() ? Formula::F : Formula::T);
         return;
     }
 
@@ -197,10 +172,10 @@ bool getResultOfRow(bool left, bool right, Formula::Kind boolOperator) {
     }
 }
 
-void calculateFormula(Formula formula, vector<unordered_map<Formula, Formula> > variablesCombinations,
+void calculateFormula(Formula formula, const vector<unordered_map<Formula, Formula> > &variablesCombinations,
                       vector<int> &resultVector) {
     vector<Formula> stackFormulas;
-    for (auto currentRow : variablesCombinations) {
+    for (const auto &currentRow : variablesCombinations) {
         vector<Formula> edited;
         getResultForCurrentRow(currentRow, formula, stackFormulas);
 
@@ -224,21 +199,16 @@ Formula getNeededVar(unordered_map<Formula, Formula> &map, int &at) {
 }
 
 void insertOrUpdateNeeded(unordered_map<Formula, Formula> &neededs, const Formula &needed, const Formula &site) {
-    bool found = false;
     for (auto &it : neededs) {
         if (it.first.kind() == needed.kind() && it.first.var() == needed.var()) {
             neededs.erase(it.first);
         }
     }
-
-    if (!found) {
-        neededs.insert({needed, site});
-    }
-
+    neededs.insert({needed, site});
 }
 
 void makePrint(unordered_map<Formula, Formula> &neededs, vector<int> &mainQuery, const Formula &site, Node nodeRoot,
-               Node node, string value) {
+               Node node, const string &value) {
     unordered_map<Formula, Formula> temp;
     for (auto &it : neededs) {
         temp.insert(it);
@@ -275,48 +245,6 @@ void makePrint(unordered_map<Formula, Formula> &neededs, vector<int> &mainQuery,
             }
         }
     }
-
-
-//    for (int &i : mainQuery) {
-//        Formula need = getNeededVar(neededs, i);
-//        if (need.kind() != Formula::F.kind()) {
-//            int needSize = neededs.size();
-//            int index = 0;
-//            while (index < needSize) {
-//                for (auto &it : neededs) {
-//                    if (it.first.kind() == need.kind() && it.first.var() == need.var()) {
-//                        if (index == needSize - 1) {
-//                            if (it.second.kind() == Formula::T.kind()) {
-//                                nodeRoot.high = &node;
-//                                cout << "([x" << it.first.var() << "] ---> high [" << value << "])";
-//                            } else {
-//                                nodeRoot.low = &node;
-//                                cout << "([x" << it.first.var() << "] ---> low [" << value << "])";
-//                            }
-//                        } else {
-//                            if (site.kind() == Formula::T.kind()) {
-//                                nodeRoot.high = &node;
-//                                cout << "[x" << need.var() << "] ---> high ";
-//                            } else {
-//                                nodeRoot.low = &node;
-//                                cout << "[x" << need.var() << "] ---> low ";
-//                            }
-//                        }
-//                        index++;
-//                    }
-//                }
-//            }
-//            cout << endl;
-//        }
-//    }
-
-//    if (site.kind() == Formula::T.kind()) {
-//        nodeRoot.high = &node;
-//        cout << "([x" << nodeRoot.var << "] ---> high [" << value << "])" << endl;
-//    } else {
-//        nodeRoot.low = &node;
-//        cout << "([x" << nodeRoot.var << "] ---> low [" << value << "])" << endl;
-//    }
 }
 
 void addRootLinks(vector<Node> &roots, vector<int> &resultVectorTemp, const Formula &site, vector<int> &query,
@@ -337,37 +265,14 @@ void addRootLinks(vector<Node> &roots, vector<int> &resultVectorTemp, const Form
     if (isOne) {
         Node node = Node(10000000, nullptr, nullptr);
         makePrint(neededs, mainQuery, site, nodeRoot, node, "1");
-//        if (site.kind() == Formula::T.kind()) {
-//            nodeRoot.high = &node;
-//            cout << "([x" << nodeRoot.var << "] ---> high [1])" << endl;
-//        } else {
-//            nodeRoot.low = &node;
-//            cout << "([x" << nodeRoot.var << "] ---> low [1])" << endl;
-//        }
     }
     if (isZero) {
         Node node = Node(-10000000, nullptr, nullptr);
         makePrint(neededs, mainQuery, site, nodeRoot, node, "0");
-//        if (site.kind() == Formula::T.kind()) {
-//            nodeRoot.high = &node;
-//            cout << "([x" << nodeRoot.var << "] ---> high [0])" << endl;
-//        } else {
-//            nodeRoot.low = &node;
-//            cout << "([x" << nodeRoot.var << "] ---> low [0])" << endl;
-//        }
     }
     if (!isZero && !isOne) {
         Formula need = getNeededVar(map, query.at(level));
         Node newRoot = Node(need.var(), nullptr, nullptr);
-//        makePrint(neededs, mainQuery, site, nodeRoot, newRoot, "0");
-//        if (site.kind() == Formula::T.kind()) {
-//            nodeRoot.high = &newRoot;
-//            cout << "([x" << nodeRoot.var << "] ---> high [x" << need.var() << "]) ---> ";
-//        } else {
-//            nodeRoot.low = &newRoot;
-//            cout << "([x" << nodeRoot.var << "] ---> low [x" << need.var() << "]) ---> ";
-//        }
-//        query.erase(query.begin());
         roots.push_back(newRoot);
         insertOrUpdateNeeded(neededs, need, Formula::T);
 
@@ -423,21 +328,21 @@ getRootsRecur(Formula &formula1, vector<int> &query, vector<unordered_map<Formul
 
 
 int main() {
-    Formula formula1 = x(1) || x(2) && x(3);
-//    Formula splitBy = x(1);
+//    Исходная формула
+//    Formula formula1 = x(1) || x(2) && x(3);
+//    Formula formula1 = x(1) != x(2) != x(3);
+    Formula formula1 = (x(1) >> (x(2) && x(3))) >> !(x(1) || (x(2) >> x(3)));
+//    Formula formula2 = (x(1) || (x(2) >> x(3)));
+//    Порядок переменных (плиз индексы теже ставьте, заглушки и проверки не ставил)
     vector<int> query = vector<int>{2, 1, 3};
+//    vector<int> query = vector<int>{0, 1};
     vector<int> mainQuery = query;
-//    const Formula &formula1 =  x(0) >>  x(1);
-//    const Formula &formula2 = !x(1) >> !x(0);
-//    const Formula &formula3 = formula1 == formula2;
-//    const Formula &formula4 = T;
+//    Formula formula1 = x(0) >> x(1);
+//     Formula formula1 = !x(1) >> !x(0);
+//     Formula formula1 = T;
 //
     std::cout << "Formulae: " << std::endl;
     std::cout << formula1 << std::endl;
-//    std::cout << formula2 << std::endl;
-//    std::cout << formula3 << std::endl;
-//    std::cout << formula4 << std::endl;
-//
     Bdd bdd;
 
     unordered_set<Formula> variables;
@@ -474,23 +379,14 @@ int main() {
     cout << endl;
     cout << endl;
 
-//    try {
-//        auto temp = variablesCombinations.at(0).at(splitBy);
-//    } catch (...) {
-//        cout << "Разложение по неизвестному элементу!!!!!";
-//    }
-
     int level = 0;
     vector<Node> roots;
     unordered_map<Formula, Formula> neededs;
     Formula needed = getNeededVar(variablesCombinations.at(0), query.at(level));
-//    query.erase(query.begin());
     Node root = bdd.create(needed);
     roots.push_back(root);
     insertOrUpdateNeeded(neededs, needed, Formula::T);
     roots = getRootsRecur(formula1, query, variablesCombinations, roots, neededs, mainQuery, 1);
-
-    int i = 1;
 
     return 0;
 }
